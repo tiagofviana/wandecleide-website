c451d5591f92cf218995549e937f52d431b1e9c5
Tiago FViana <tiagofviana.dev@gmail.com>
2023-12-28 20:39:04 -0300

    Adding maskable icon and redirect urls

--- src/core/urls.py
+++ src/core/urls.py
@@ -14,7 +14,13 @@ app_name = 'core'
 
 urlpatterns = [
     path('messages/', views.MessagesTemplateView.as_view(), name='messages'),
+    # FAVICONS
     path('favicon.ico', lambda request: redirect('/static/core/favicons/favicon.ico')),
+    path('android-chrome-512x512.png', lambda request: redirect('/static/core/favicons/android-chrome-512x512.png')),
+    path('android-chrome-192x192.png', lambda request: redirect('/static/core/favicons/android-chrome-192x192.png')),
+    path('favicon-32x32.png', lambda request: redirect('/static/core/favicons/favicon-32x32.png')),
+    path('favicon-16x16.png', lambda request: redirect('/static/core/favicons/favicon-16x16.png')),
+    path('site.webmanifest', lambda request: redirect('/static/core/favicons/site.webmanifest')),
     # SEO
     path('sitemap.xml', sitemap,{'sitemaps': SITEMAPS}, name='django.contrib.sitemaps.views.sitemap'),
 ]

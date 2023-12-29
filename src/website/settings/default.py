0bd1909f9366220cea6407fefdd022cf0d308417
Tiago FViana <tiagofviana.dev@gmail.com>
2023-12-28 23:06:17 -0300

    Fixing missing CSP_RULES

--- src/website/settings/default.py
+++ src/website/settings/default.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'users',
 ]
 
+
 SITE_ID = 1
 
 
@@ -55,6 +56,19 @@ MIDDLEWARE = [
     # Custom
     'website.middlewares.csp.ContentSecurityPolicyMiddleware',
 ]
+
+# Content Security Policy
+CSP_RULES = """
+    child-src 'none'; 
+    worker-src 'none';
+    font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com;
+    script-src  'self';
+    object-src 'none';
+    form-action 'self';
+    base-uri 'self';
+    upgrade-insecure-requests;
+    block-all-mixed-content;
+"""
 
 ROOT_URLCONF = 'website.urls'
 

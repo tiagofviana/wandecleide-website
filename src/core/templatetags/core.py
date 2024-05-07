5efc462ae67545eac3357b4cfa8b596366f6fd56
Tiago FViana <tiagofviana.dev@gmail.com>
2024-05-07 15:18:02 -0300

    Adding effect to show current page

--- src/core/templatetags/core.py
+++ src/core/templatetags/core.py
@@ -1,6 +1,7 @@
 from django import template
 from django.utils import translation
 from django.urls import resolve, reverse
+from django.utils.translation import gettext as _
 
 register = template.Library()
 
@@ -12,3 +13,12 @@ def translate_current_url(context, language_code: str):
     url = reverse(f"{view.app_name}:{view.url_name}", args=view.args, kwargs=view.kwargs)
     translation.activate(request_language)
     return url
+
+@register.simple_tag(takes_context=False)
+def menu_list_items() -> tuple:
+    return (
+        (_('Home'), reverse('users:index')),
+        (_('About'), reverse('users:about')),
+        (_('My work'), reverse('users:my-work')),
+        (_('Contact'), reverse('users:contact')),
+    )
